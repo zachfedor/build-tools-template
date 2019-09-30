a1f23cc31a80b03035334e58baed37e522282e4f
Zach Fedor <zachfedor@gmail.com>
2019-09-30 16:07:01 -0400

    Add HTML and CSS linting

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,46 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
+const { parallel, src, series, watch } = require('gulp');
+const browserSync = require('browser-sync').create();
+const htmlLint = require('gulp-html-linter');
+const cssLint = require('gulp-csslint');
+
 
 // Static server
-gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        }
-    });
+const serve = () => {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+};
+const reload = (cb) => {
+  browserSync.reload();
+  cb();
+}
 
-    gulp.watch("**/*.css").on('change', browserSync.reload);
-    gulp.watch("**/*.html").on('change', browserSync.reload);
-});
+// HTML Linting
+const html = (cb) => {
+  src(['**/*.html', '!node_modules/**/*.html' ])
+    .pipe(htmlLint())
+    .pipe(htmlLint.format());
 
+  cb();
+};
 
+// CSS Linting
+const css = (cb) => {
+  src(['**/*.css', '!node_modules/**/*.css' ])
+    .pipe(cssLint())
+    .pipe(cssLint.formatter());
+  
+  cb();
+}
+
+exports.css = css;
+exports.html = html;
+exports.lint = parallel(html, css);
+exports.default = () => {
+  serve();
+
+  watch(['**/*.css', '!node_modules/**/*.css' ], series(css, reload));
+  watch(['**/*.html', '!node_modules/**/*.html' ], series(html, reload));
+};
